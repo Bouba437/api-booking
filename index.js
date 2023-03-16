a4efc2ec4eedc1bf465f36b6d4275b99b0c3c2ef
Bouba <diarraboubaca@yahoo.fr>
2023-03-16 02:37:58 +0100

    Création du Schema et du model User, création du controller register

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import morgan from "morgan";
 import  path from "path"; // ** vient directement de node
 import { fileURLToPath } from "url";
 import helmet from "helmet";
+import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +34,9 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
